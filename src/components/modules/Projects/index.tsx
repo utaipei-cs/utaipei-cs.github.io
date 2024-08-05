350b67a00e3355171382e3db4f008378c2030429
JEFF Liao <jeffliao92316046@gmail.com>
2024-08-06 00:29:11 +0800

    fix : 斑馬

--- src/components/modules/Projects/index.tsx
+++ src/components/modules/Projects/index.tsx
@@ -18,9 +18,9 @@ const Repos = async () => {
   return (
     <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 col-span-1 md:col-span-2 lg:col-span-3">
       {data
-        ? data.map(({ node }) => {
+        ? data.map(({ node }, index) => {
             if (!node) return null;
-            return <Repository key={node.id} {...node} />;
+            return <Repository key={node.id} node={node} index={index} />;
           })
         : null}
     </div>
--- src/components/ui/Repository/index.tsx
+++ src/components/ui/Repository/index.tsx
@@ -2,41 +2,49 @@ import ForkIcon from 'components/ui/Icons/Fork';
 import StarIcon from 'components/ui/Icons/Star';
 import { Repository } from 'generated/graphql';
 
-const Repository = ({ name, description, stargazers, forkCount, languages, url }: Repository) => (
-  <a
-    className="bg-white shadow-md dark:bg-zinc-900 rounded-sm p-4 h-full overflow-hidden flex flex-col justify-between"
-    href={url}
-    target="_blank"
-    rel="noreferrer"
-  >
-    <div>
-      <h3 className="text-black dark:text-white font-semibold text-xl">{name}</h3>
-      <p className="text-gray-700 dark:text-gray-200 mb-4 text-base">{description}</p>
-    </div>
-    <div className="flex justify-between flex-col">
-      {languages?.nodes && languages.nodes.length > 0 && (
-        <div className="flex justify-between flex-row mb-3">
-          <div>
-            {languages.nodes.map((item) => (
-              <span className="text-gray-700 dark:text-gray-200 italic mr-2 last:mr-0 text-xs" key={item?.id}>
-                {item?.name}
-              </span>
-            ))}
+interface RepositoryX {
+  node: Repository;
+  index: number;
+}
+
+const RepositoryPage = ({ node, index }: RepositoryX) => {
+  // Use the same background color and text color for all items
+  const bgColorClass =
+    'bg-white dark:bg-gray-800 text-brand-primary dark:text-gray-200 shadow-md rounded-lg p-4 h-full overflow-hidden flex flex-col justify-between';
+  // Use text-gray-200 for the icon color
+  const iconColorClass = 'text-brand-primary dark:text-gray-200';
+
+  return (
+    <a className={bgColorClass} href={node.url} target="_blank" rel="noreferrer">
+      <div>
+        <h3 className="font-semibold text-xl">{node.name}</h3>
+        <p className="mb-4 text-base">{node.description}</p>
+      </div>
+      <div className="flex justify-between flex-col">
+        {node.languages?.nodes && node.languages.nodes.length > 0 && (
+          <div className="flex justify-between flex-row mb-3">
+            <div>
+              {node.languages.nodes.map((item) => (
+                <span className="italic mr-2 last:mr-0 text-xs" key={item?.id}>
+                  {item?.name}
+                </span>
+              ))}
+            </div>
+          </div>
+        )}
+        <div className="flex items-center">
+          <div className="flex items-center mr-2">
+            <StarIcon className={iconColorClass} />
+            <span className={`ml-2 ${iconColorClass}`}>{node.stargazers.totalCount}</span>
+          </div>
+          <div className="flex items-center">
+            <ForkIcon className={iconColorClass} />
+            <span className={`ml-2 ${iconColorClass}`}>{node.forkCount}</span>
           </div>
         </div>
-      )}
-      <div className="flex items-center">
-        <div className="flex items-center mr-2">
-          <StarIcon className="text-black dark:text-white" />
-          <span className="ml-2 text-black dark:text-white">{stargazers.totalCount}</span>
-        </div>
-        <div className="flex items-center">
-          <ForkIcon className="text-black dark:text-white" />
-          <span className="ml-2 text-black dark:text-white">{forkCount}</span>
-        </div>
       </div>
-    </div>
-  </a>
-);
+    </a>
+  );
+};
 
-export default Repository;
+export default RepositoryPage;
