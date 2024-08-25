013157d8d48721f26d81e236c6d5e12b20ba0191
nickchengtw <33611456+nickchengtw@users.noreply.github.com>
2024-08-25 15:41:31 +0800

    Add logo & update text content

--- src/components/ui/theme/Header/Navbar/index.tsx
+++ src/components/ui/theme/Header/Navbar/index.tsx
@@ -3,8 +3,12 @@ import NavbarLinks from 'components/ui/theme/Header/NavbarLinks';
 
 const Navbar = () => (
   <div className="container flex items-center justify-between p-6">
-    <Link href="/" className="typography dark:text-white text-black">
-      PUT LOGO HERE???
+    <Link href="/" className="typography dark:text-white text-black flex">
+      <img src="/assets/logo.png" alt="Logo" class="w-16 h-16" />
+			<div className="flex flex-col justify-center">
+      <span className="ml-4 text-2xl font-bold text-gray-600">臺北市立大學</span>
+			<span className="ml-4 text-lg font-bold text-gray-600">資訊科學系系學會</span>
+			</div>
     </Link>
     <NavbarLinks desktop />
   </div>
